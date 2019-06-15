3e5982eee2da3042a57bee4e41f39ce07ca493ce
Boaz Sze <boaz.sze@gmail.com>
2019-06-16 03:40:35 +0800

    Messaged specific start channel on ready

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const Discord = require('discord.js')
-const url = require('url')
-const http = require('http')
 
 const config = require('./config/config.json')
 
@@ -8,16 +6,8 @@ const client = new Discord.Client()
 
 client.on('ready', () => {
   console.log(`Bobot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)
-  const clientData = JSON.stringify(client)
-  const app = http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/html" })
-    response.write(JSON.stringify(clientData))
-    response.end()
-  });
-  app.listen(8000)
-  console.log(`Bobot served on http://localhost:8000`)
 
-  console.log(clientData)
+  client.channels.get(config.DiscordBotInitialTextChannel).send(`hi, i am active!`)
 });
 
 client.on('message', (user, userID, channelID, message, evt) => {
