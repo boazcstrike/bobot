1f3f2ef81234c4d9f3d347fb94b10eabfca6801b
Boaz Sze <boaz.sze@gmail.com>
2019-06-16 03:22:20 +0800

    Tried to serve to web server

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const Discord = require('discord.js')
+const url = require('url')
+const http = require('http')
+
 const config = require('./config/config.json')
 
 const client = new Discord.Client()
 
 client.on('ready', () => {
   console.log(`Bobot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`)
+  const clientData = JSON.stringify(client)
+  const app = http.createServer((request, response) => {
+    response.writeHead(200, { "Content-Type": "text/html" })
+    response.write(JSON.stringify(clientData))
+    response.end()
+  });
+  app.listen(8000)
+  console.log(`Bobot served on http://localhost:8000`)
+
+  console.log(clientData)
 });
 
 client.on('message', (user, userID, channelID, message, evt) => {
