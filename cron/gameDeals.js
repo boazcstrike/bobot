bcdbc7111954d803d139c859853d7a51da83011b
Boaz Sze <boaz.sze@gmail.com>
2019-06-18 00:40:01 +0800

    Ran on test channel

--- cron/gameDeals.js
+++ cron/gameDeals.js
@@ -4,8 +4,8 @@ var CronJob = require('cron').CronJob
 const config = require('../config/config.json')
 const getReddit = require('./_getReddit')
 
-const gameDealsChannel = config.GameDealsTextChannel
-// const gameDealsChannel = config.TestTextChannel
+// const gameDealsChannel = config.GameDealsTextChannel
+const gameDealsChannel = config.TestTextChannel
 const client = new Discord.Client()
 const defaultLimit = 5 // default number of post to send
 const time = 12 // default hours
@@ -13,7 +13,6 @@ const time = 12 // default hours
 client.on('ready', () => {
     console.log(`game deals initiated, will run every ${time} hours.`)
     newNews()
-    
     const gamedealsJob = new CronJob('0 0 */'+time+' * *', function () {
         console.log(`Game deals running every ${time} hours...`)
         newNews()
--- cron/gamerNews.js
+++ cron/gamerNews.js
@@ -2,9 +2,10 @@ const Discord = require('discord.js')
 var CronJob = require('cron').CronJob
 
 const config = require('../config/config.json')
-const { getRedditNews } = require('./_getReddit')
+const getReddit = require('./_getReddit')
 
-const gamerChannel = config.GamerTextChannel
+// const gamerChannel = config.GamerTextChannel
+const gamerChannel = config.TestTextChannel
 const client = new Discord.Client()
 const defaultLimit = 5 // default number of post to send
 const time = 8 // default hours
@@ -12,7 +13,6 @@ const time = 8 // default hours
 client.on('ready', () => {
     console.log(`gamer news initiated, will run every ${time} hours...`)
     topNews()
-
     const gamernewsJob = new CronJob('0 0 */'+time+' * *', function () {
         console.log(`running every ${time} hours...`)
         topNews()
@@ -22,8 +22,8 @@ client.on('ready', () => {
 });
 
 async function topNews() {
-    getRedditNews('gaming', 'top', gamerChannel, defaultLimit)
-    getRedditNews('GirlGamers', 'top', gamerChannel, defaultLimit)
+    getReddit.getPosts(client, 'gaming', 'top', gamerChannel, defaultLimit)
+    getReddit.getPosts(client, 'GirlGamers', 'top', gamerChannel, defaultLimit)
 }
 
 client.login(config.DiscordBotToken)
